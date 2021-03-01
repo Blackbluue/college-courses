96e309d46e4e717d4082c33983d8d1cbc516ea77
Raymone Miller <miller_raymone@hotmail.com>
2021-03-01 09:08:07 -0500

    Update MillerRaymone_Asgn1.java

--- cmis141/Week1/MillerRaymone_Asgn1/src/main/java/com/miller/millerraymone_asgn1/MillerRaymone_Asgn1.java
+++ cmis141/Week1/MillerRaymone_Asgn1/src/main/java/com/miller/millerraymone_asgn1/MillerRaymone_Asgn1.java
@@ -13,12 +13,49 @@ import java.util.Scanner;
  */
 public class MillerRaymone_Asgn1 {
     /*
-    (1) Prompts a user to enter student id, current class grade in GPA format (e.g. 3.5), current class
-number of credits, overall GPA, and overall number of credits (use Scanner for input).
-    */
-    int student_id;
-    double curr_grade;
-    int curr_creds;
-    double ovr_grade;
-    Scanner sc = new Scanner(System.in);
+    (3) Displays the input data along with new GPA to the console
+
+    Enter student id: 6543
+    Enter current class grade in GPA format (e.g. 3.5): 3.5
+    Enter current class number of credits: 3
+    Enter overall GPA: 4.0
+    Enter overall number of credits: 12
+    STUDENT DATA:
+    Student id: 6543
+    Current class GPA: 3.5
+    Current class credits:3
+    Overall GPA: 3.5
+    Overall credits:3
+    NEW GPA: 3.9
+
+     */
+    public static void main(String[] args) {
+        int student_id;
+        double curr_gpa;
+        int curr_creds;
+        double ovr_gpa;
+        int ovr_creds;
+        double new_gpa;
+        try( Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter student ID: ");
+            student_id = sc.nextInt();
+            System.out.print("Enter current class grade in GPA format: ");
+            curr_gpa = sc.nextDouble();
+            System.out.print("Enter current class number of credits: ");
+            curr_creds = sc.nextInt();
+            System.out.print("Enter overall GPA: ");
+            ovr_gpa = sc.nextDouble();
+            System.out.print("Enter overall number of credits: ");
+            ovr_creds = sc.nextInt();
+        }
+        new_gpa = (curr_gpa * curr_creds) + (ovr_gpa * ovr_creds)
+                / (curr_creds + ovr_creds);
+        System.out.println("\nSTUDENT DATA:");
+        System.out.printf("Student ID: %d", student_id);
+        System.out.printf("Current class GPA: %f", curr_gpa);
+        System.out.printf("Current class credits: %d", curr_creds);
+        System.out.printf("Overall GPA: %f", ovr_gpa);
+        System.out.printf("Overall credits: %d", ovr_creds);
+        System.out.printf("NEW GPA: %f", new_gpa);
+    }
 }
