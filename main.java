c8617124fe9111b4d7d310058b39916d9b45cf6b
Raymone Miller <miller_raymone@hotmail.com>
2021-03-02 02:38:25 +0000

    asignment1

--- main.java
+++ main.java
@@ -3,7 +3,7 @@
  * Class/Section CMIS 141/6980
  * Date: (03/01/2021)
  */
-package com.miller.millerraymone_asgn1;
+//package com.miller.millerraymone_asgn1;
 
 import java.util.Scanner;
 
@@ -11,14 +11,16 @@ import java.util.Scanner;
  *
  * @author miller
  */
-public class MillerRaymone_Asgn1 {
+public class main {
     public static void main(String[] args) {
+        // Define variables
         int student_id;
         double curr_gpa;
         int curr_creds;
         double ovr_gpa;
         int ovr_creds;
         double new_gpa;
+        // Initialize variables with use input
         try( Scanner sc = new Scanner(System.in)) {
             System.out.print("Enter student ID: ");
             student_id = sc.nextInt();
@@ -35,10 +37,10 @@ public class MillerRaymone_Asgn1 {
                 / (curr_creds + ovr_creds);
         System.out.println("\nSTUDENT DATA:");
         System.out.printf("Student ID: %d\n", student_id);
-        System.out.printf("Current class GPA: %.2f\n", curr_gpa);
+        System.out.printf("Current class GPA: %f\n", curr_gpa);
         System.out.printf("Current class credits: %d\n", curr_creds);
-        System.out.printf("Overall GPA: %.2f\n", ovr_gpa);
+        System.out.printf("Overall GPA: %f\n", ovr_gpa);
         System.out.printf("Overall credits: %d\n", ovr_creds);
-        System.out.printf("NEW GPA: %.2f\n", new_gpa);
+        System.out.printf("NEW GPA: %f\n", new_gpa);
     }
 }
