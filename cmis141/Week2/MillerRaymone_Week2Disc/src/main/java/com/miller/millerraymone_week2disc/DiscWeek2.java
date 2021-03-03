f890ff917a337a1741187d201f62696bb6a05b12
Raymone Miller <miller_raymone@hotmail.com>
2021-03-03 04:29:47 +0000

    Finish response for week 2 discussion

--- cmis141/Week2/MillerRaymone_Week2Disc/src/main/java/com/miller/millerraymone_week2disc/DiscWeek2.java
+++ cmis141/Week2/MillerRaymone_Week2Disc/src/main/java/com/miller/millerraymone_week2disc/DiscWeek2.java
@@ -3,14 +3,15 @@
  * 
  * Week 2 - Input and Arithmetic
  * 
- * A Java program which prompts a user for two numerical values as input, 
+ * A Java program which prompts SideA user for two numerical values as input, 
  * does some calculation on them and then prints the result 
- * along with a meaningful message to the console.
+ * along with SideA meaningful message to the console.
  *
  */
 
 import java.util.Scanner; // Using Scanner for user input.
-import java.lang.Math;    // Using Math class for square root operation.
+// import java.lang.Math;    // Using Math class for square root operation.
+// Don't need to import from java.lang
 
 public class DiscWeek2 {
 
@@ -21,6 +22,7 @@ public class DiscWeek2 {
 		// Ask for user input.
 		System.out.println("Grotans,John CMIS 141/2212 (02/22/2021)\n");
 		System.out.println("JOHN'S PYTHAGOREAN THEOREM CALCULATOR!"); // Program title.
+        System.out.println("AND RAYMONE'S TRIANGULAR PRISM VOLUME CALCULATOR!");
 		System.out.println("   |\\");
 		System.out.println("   | \\");
 		System.out.println(" A |  \\ C");
@@ -33,15 +35,26 @@ public class DiscWeek2 {
 		
 		System.out.print("Enter integer number for side B of right triangle: ");
 		int SideB = scanint.nextInt(); // Wait for user to type in integer number.
+
+        System.out.print("Enter integer number for height of prism");
+        int height = scanint.nextInt();
 		
 		// Calculate the length of side C.
 		double SideC = Math.sqrt((SideA * SideA) + (SideB * SideB)); //Pythagorean theorem.
+
+        // Calculate the volume of the prism.
+        double volume = 0.25 * height * Math.sqrt(
+            (SideA + SideB + SideC) * (SideB + SideC − SideA)
+            * (SideC + SideA − SideB) * (SideA + SideB − SideC));
 				
 		// Display the output.
 		System.out.println("The length of side C is: " + SideC); // Print result of calculation.
-		System.out.println(""); // Print an empty line.
+		System.out.println(); // Print an empty line.
 		System.out.println("Pythagorean theorem states that " + SideC + " = the sqaure root of " + 
 				SideA + "^2" + " + " + SideB + "^2"); // Print student data.
+        System.out.printf("Volume of the triangular prism is 1/4 * %d * " + 
+            "√((%d + %d + %d)(%d + %d - %d)(%d + %d - %d)(%d + %d - %d))",
+            SideA, SideB, SideC, SideB, SideC, SideA, SideC, SideA, SideB, SideA, SideB, SideC);
 		
 		// Close the scanners.
 		scanint.close();
