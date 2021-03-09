5496ae1fc030c53d0014d3c9ee162e2183f1672b
blackbluue <miller_raymone@hotmail.com>
2021-03-09 21:22:44 +0000

    Add comments for assignment 2

--- cmis141/Week2/MillerRaymone_Asgn2/src/main/java/com/miller/millerraymone_asgn2/MillerRaymone_Asgn2.java
+++ cmis141/Week2/MillerRaymone_Asgn2/src/main/java/com/miller/millerraymone_asgn2/MillerRaymone_Asgn2.java
@@ -12,11 +12,21 @@ import java.util.Scanner;
 public class MillerRaymone_Asgn2 {
 
     public static void main(String[] args) {
+        /*
+        begin 'try' clause
+        'catch' clauses to handle exceptions
+        */
         try {
+            // get user input
             Object[] input = get_user_input();
             int num1 = (int) input[0];
             int num2 = (int) input[1];
             char symbol = (char) input[2];
+            /*
+            switch statement to control program flow
+            4 cases for each arithmetic operator
+            default case for catch all
+            */
             switch(symbol) {
                 case '+':
                     System.out.printf("%d %c %d = %d\n",
@@ -37,32 +47,53 @@ public class MillerRaymone_Asgn2 {
                         System.out.printf("%d %c %d = %f\n",
                             num1, symbol, num2, (double) num1 / num2);
                     break;
-                default:
+                default:  // this case should never happen
                     System.out.println("Error parsing input");
             }
         } catch(ArrayIndexOutOfBoundsException e) {
+            /*
+            user entered less than 2 numbers
+            note that 3+ numbers are ok ,extras are ignored
+            */
             System.out.println("You did not enter enough numbers.");
         } catch(NumberFormatException e) {
+            // user entered a non-number
             System.out.println(e.getMessage() + " is not a number");
         } catch(RuntimeException e) {
+            // user entered an unknown operator
             System.out.println(e.getMessage());
         }
     }
 
+    /**
+     * 
+     */
     public static Object[] get_user_input() {
-        System.out.print("Enter two integer numbers separated by space: ");
+        // create scanner for user input
         Scanner sc = new Scanner(System.in);
 
+        /*
+        collect and validate user input for numbers
+        Integer.parseInt() will throw NumberFormatException if wrong input
+        */
+        System.out.print("Enter two integer numbers separated by space: ");
         String[] input = sc.nextLine().split(" ");
         int num1 = Integer.parseInt(input[0]);
         int num2 = Integer.parseInt(input[1]);
 
+        /*
+        collect and validate user input for operator
+        'if' conditional will generate RuntimeException if unknown operator
+        */
         System.out.print("Enter operation symbol (+, -, *, or /): ");
         char symbol = sc.nextLine().charAt(0);
         if ("+-*/".indexOf(symbol) == -1)
             throw new RuntimeException(
                 String.format("'%c' is not a valid operation", symbol));
 
+        /*
+        return an array to hold all input since java cannot unpack varaibles
+        */
         return new Object[]{num1, num2, symbol};
     }
 }
