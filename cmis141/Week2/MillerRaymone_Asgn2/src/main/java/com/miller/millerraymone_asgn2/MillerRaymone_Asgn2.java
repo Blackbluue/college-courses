876174b125e1e5f9bcbc86632a34fbdad7e81543
Raymone Miller <miller_raymone@hotmail.com>
2021-03-08 23:29:56 -0500

    Create MillerRaymone_Asgn2.java

---
+++ cmis141/Week2/MillerRaymone_Asgn2/src/main/java/com/miller/millerraymone_asgn2/MillerRaymone_Asgn2.java
@@ -0,0 +1,14 @@
+/**
+ * File: Assignment4.java
+ * Author: Miller, Raymone
+ * Class:  CMIS 141 – Assignment 4
+ * Creation Date: (08MAR21)
+ * Description: Prompts and evaluates user input for math equation.
+ */
+package com.miller.millerraymone_asgn2;
+public class MillerRaymone_Asgn2 {
+
+    public static void main(String[] args) {
+
+    }
+}
