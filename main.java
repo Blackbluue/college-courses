8cd9bcb281a104efa76791f4ceea531dd2071525
Raymone Miller <miller_raymone@hotmail.com>
2021-03-17 20:22:09 +0000

    Start week 5 discussion

--- main.java
+++ main.java
@@ -7,27 +7,50 @@
  */
 package com.miller.millerraymone_asgn3;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class MillerRaymone_Asgn3 {
 
     public static void main(String[] args) {
-        try(Scanner sc = new Scanner(System.in)) {
-            String test;
-            do {
-                System.out.print("Test: ");
-                test = sc.nextLine();
-                System.out.println(test);
-                temp(sc);
-            }while(!test.equals("end"));
+        Map<String, Integer> input = getInput();
+        String message = "WELCOME";
+
+        List<String> matHalf = new ArrayList<>();
+        for(int row = 0; row < input.get("height") / 2; row++) {
+            String patternString = repeat(".|.", (row * 2 + 1));
+            String lineString = repeat("-", (input.get("width") - patternString.length()) / 2);
+            matHalf.add(lineString + patternString + lineString);
+        }
+
+        for(String pattern: matHalf) {
+            System.out.println(pattern);
+        }
+        System.out.println(repeat("-", input.get("width") / 2 - (message.length() / 2)) + message + repeat("-", input.get("width") / 2 - (message.length() / 2)));
+        Collections.reverse(matHalf);
+        for(String pattern: matHalf) {
+            System.out.println(pattern);
         }
     }
 
-    public static void temp(Scanner sc) {
-        System.out.print("Inner Test: ");
-        String test = sc.nextLine();
-        System.out.println(test);
+    public static String repeat(String str, int count) {
+        return new String(new char[count]).replace("\0", str);
+    }
+
+    public static Map<String, Integer> getInput() {
+        System.out.print("How high should the mat be? Enter an odd number: ");
+        try(Scanner sc = new Scanner(System.in)) {
+            int height;
+            boolean goodInput = false;
+            do {
+                height = sc.nextInt();
+                if(height % 2 == 0)
+                    System.out.print("Height must be an odd number: ");
+                else if(height <= 3)
+                    System.out.print("Height must be greater than 3: ");
+                else
+                    goodInput = true;
+            }while(goodInput == false);
+            return Map.of("height", height, "width", height * 3);
+        }
     }
 }
