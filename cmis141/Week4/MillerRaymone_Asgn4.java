cc1a9d108e90971c77e5079d2ffa36ae84549820
blackbluue <miller_raymone@hotmail.com>
2021-03-19 20:14:28 +0000

    Finish code for assignment 4

--- cmis141/Week4/MillerRaymone_Asgn4.java
+++ cmis141/Week4/MillerRaymone_Asgn4.java
@@ -6,40 +6,70 @@
  * Description: 
  */
 
- package com.miller.millerraymone_asgn4;
+package com.miller.millerraymone_asgn4;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
  public class MillerRaymone_Asgn4 {
     public static void main(String[] args) {
-        buildTriangle(6);
-        System.out.println();
-        buildDiamond(6);
+        int input = getInput(
+            "How high should the triangle be? ", "Height must be positive: ");
+        buildTriangle(input);
+        input = getInput(
+            "How long should each side be? ", "length must be positive: ");
+        buildDiamond(input);
     }
 
     public static void buildTriangle(int rows) {
         for(int rowNum = 0; rowNum < rows; rowNum++) {
-            for(int rowLength = 0; rowLength <= rowNum; rowLength++) {
+            for(int rowLength = 0; rowLength <= rowNum; rowLength++)
                 System.out.print("#");
-            }
             System.out.println();
         }
     }
+
     public static void buildDiamond(int sideLength) {
-        for(int rowNum = 0; rowNum < sideLength * 2 - 1; rowNum++) {
-            if(rowNum <= sideLength) {
-                for(int rowLength = 0; rowLength < sideLength + rowNum; rowLength++)
-                    if(rowLength <= sideLength - rowNum)
-                        System.out.print(" ");
-                    else
-                        System.out.print("#");
-            } else {
-                for(int rowLength = sideLength - rowNum; rowLength > 0; rowLength--) {
-                    if(rowLength <= sideLength - rowNum)
-                        System.out.print("-");
-                    else
-                        System.out.print("#");
-                }
-            }
+        for(int rowNum = 0; rowNum < sideLength - 1; rowNum++) {
+            for(int rowLength = 0; rowLength <= sideLength + rowNum; rowLength++)
+                if(rowLength < sideLength - rowNum)
+                    System.out.print(" ");
+                else
+                    System.out.print("#");
             System.out.println();
         }
+        for(int rowNum = sideLength - 1; rowNum >= 0; rowNum--) {
+            for(int rowLength = 0; rowLength <= sideLength + rowNum; rowLength++)
+                if(rowLength < sideLength - rowNum)
+                    System.out.print(" ");
+                else
+                    System.out.print("#");
+            System.out.println();
+        }
+    }
+
+    public static int getInput(String prompt, String errorPrompt) {
+        System.out.print(prompt);
+        // open scanner with auto-close try-with-resources
+        Scanner sc = new Scanner(System.in);
+        int input = 0;  // initialize input to avoid compile error
+        boolean goodInput = false;  // sentinal value
+        do {
+            try {  // wrap in try  to catch scanner exception
+                input = sc.nextInt();
+                // validate input
+                if(input <= 0)
+                    System.out.print(errorPrompt);
+                else
+                    goodInput = true;
+            } catch(InputMismatchException e) {
+                // user entered a non-number
+                System.out.print("Please enter a whole number: ");
+                sc.nextLine();  // clear scanner of bad input
+            }
+            
+        }while(goodInput == false);
+        // return input
+        return input;
     }
  }
