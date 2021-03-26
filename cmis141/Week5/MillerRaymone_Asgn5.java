d7405cbb329cc982df09df4b18fe3393d6b71586
Raymone Miller <miller_raymone@hotmail.com>
2021-03-26 13:54:43 +0000

    Finish Assignment 5

--- cmis141/Week5/MillerRaymone_Asgn5.java
+++ cmis141/Week5/MillerRaymone_Asgn5.java
@@ -18,28 +18,49 @@ public class MillerRaymone_Asgn5 {
     static final String HEIGHT = "2";
     static final String BMI = "3";
     static final String QUIT = "9";
+    static final double WEIGHT_CONVERSION = 0.45;
+    static final double HEIGHT_CONVERSION = 2.54;
+    static final int BMI_CONVERSION = 10_000;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String choice;
+        double pounds;
+        double kilos;
+        double inches;
+        double centi;
+        double bmi;
         do {
             choice = displayMenu(sc);
             switch(choice) {
                 case WEIGHT:
                     System.out.print("\tEnter weight in pounds : ");
-                    double pounds = getUserDouble(sc);
-                    double kilos = poundsToKilograms(pounds);
+                    pounds = getUserDouble(sc);
+                    kilos = poundsToKilograms(pounds);
                     System.out.printf("\t%.2f lbs = %.2f kg\n", pounds, kilos);
-                    sc.nextLine();
+                    sc.nextLine();  // clear input buffer
                     break;
                 case HEIGHT:
                     System.out.print("\tEnter height in inches : ");
-                    double inches = getUserDouble(sc);
-                    double centi = inchesToCentimeters(inches);
+                    inches = getUserDouble(sc);
+                    centi = inchesToCentimeters(inches);
                     System.out.printf("\t%.2f inches = %.2f cm\n", inches, centi);
+                    sc.nextLine();  // clear input buffer
                     break;
                 case BMI:
-                    System.out.println("\tWorking!\n");
+                    System.out.print("\tEnter weight in kg and height in centimeters separated by space : ");
+                    kilos = getUserDouble(sc);
+                    centi = getUserDouble(sc);
+                    bmi = getBMI(kilos, centi);
+                    if(bmi < 18.5)
+                        System.out.printf("\tBMI=%.2f Category=Underweight\n", bmi);
+                    else if(bmi < 25.0)
+                        System.out.printf("\tBMI=%.2f Category=Normal\n", bmi);
+                    else if(bmi < 30.0)
+                        System.out.printf("\tBMI=%.2f Category=Overweight\n", bmi);
+                    else
+                        System.out.printf("\tBMI=%.2f Category=Obese\n", bmi);
+                    sc.nextLine();  // clear input buffer
                     break;
                 case QUIT:
                     System.out.println("\nThank you for using the program. Goodbye!");
@@ -57,7 +78,7 @@ public class MillerRaymone_Asgn5 {
             System.out.println("2: Convert height in inches to centimeters");
             System.out.println("3: Calculate BMI using weight in kilograms and display category");
             System.out.println("9: Exit program");
-            System.out.print("\n Enter your selection : ");
+            System.out.print("\nEnter your selection : ");
             choice = sc.nextLine();
             switch(choice) {
                 case WEIGHT:
@@ -68,7 +89,6 @@ public class MillerRaymone_Asgn5 {
                     break;
                 default:
                     System.out.println("Not a valid choice. Choose again.");
-                    System.out.println("++" + choice + "++");
             }
         }while(badInput);
         return choice;
@@ -81,12 +101,12 @@ public class MillerRaymone_Asgn5 {
             try {
                 input = sc.nextDouble();
                 if(input <= 0)
-                    System.out.print("Please enter a positive number: ");
+                    System.out.print("\tPlease enter a positive number: ");
                 else
                     badInput = false;
             } catch(InputMismatchException e) {
                 // user entered a non-number
-                System.out.print("That is not a number. Try again: ");
+                System.out.print("\tThat is not a number. Try again: ");
                 sc.nextLine();  // clear scanner of bad input
             }
         }while(badInput);
@@ -94,10 +114,14 @@ public class MillerRaymone_Asgn5 {
     }
 
     public static double poundsToKilograms(double pounds) {
-        return pounds;
+        return pounds * WEIGHT_CONVERSION;
     }
 
     public static double inchesToCentimeters(double inches) {
-        return inches;
+        return inches * HEIGHT_CONVERSION;
+    }
+
+    public static double getBMI(double kilos, double centi) {
+        return kilos / centi / centi * BMI_CONVERSION;
     }
 }
