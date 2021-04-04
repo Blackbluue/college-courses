87e94510021b51b7f0548758e2af4030935988d1
Raymone Miller <miller_raymone@hotmail.com>
2021-04-04 03:36:52 +0000

    Add comments to week 7 discussion

--- cmis141/Week7/MillerRaymone_Week7Disc.java
+++ cmis141/Week7/MillerRaymone_Week7Disc.java
@@ -9,44 +9,69 @@
 
 package com.miller.millerraymone_week7disc;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.ArrayList;
 
 public class MillerRaymone_Week7Disc {
     public static void main(String[] args) {
-        int min = 1;
-        int max = 150;
-        int length = 10;
-        String[] arr = new String[length];
+        int min = 1;  // minimum value for random number
+        int max = 150;  // maximum value for random number
+        int length = 10;  // length of array
+        String[] arr = new String[length];  // array for random numbers
 
         System.out.println(
             "This program finds the prime factors of a " + 
             "randomly generated array of integers.\n");
+        // loop to generate random numbers and populate array
         for(int i = 0; i < length; i++)
             arr[i] = String.valueOf((int) (Math.random() * (max - min + 1) + min));
 
-        System.out.println("{" + String.join(", ", arr) + "}\n");
+        // print current contents of array
+        System.out.println(Arrays.toString(arr) + "\n");
+        // factor numbers in array
         factorInts(arr);
+        // print new contents of array
         for(String factors: arr)
             System.out.println(factors);
     }
 
+    /**
+     * Takes an array of strings, each element representing an integer.Then
+     * factors each number and replaces each element in the array with the
+     * factors.
+     *
+     * @param arr An array of strings, each representing an integer.
+     */
     public static void factorInts(String[] arr) {
+        // loop over the array, and factor each number
         for(int i = 0; i < arr.length; i++)
             arr[i] = arr[i] + " = " + factor(Integer.parseInt(arr[i]));
     }
 
+    /**
+     * Take a single integer, finding all the factors.
+     * 
+     * @param number The number to factor.
+     * @return a string stating all the factors of the number.
+     */
     public static String factor(int number) {
+        // list to hold factors of number
         List<String> factors = new ArrayList<>();
+        // for-loop to check each number less than the base number
         for(int i = 2; i< number; i++) {
+            // loop to check if i is a factor
             while(number % i == 0) {
                 factors.add(String.valueOf(i));
                 number = number / i;
             }
         }
-        if(number > 2 || factors.size() == 0)
+        // if the last number is greater than 2, or if there are no factors,
+        // add number to the factors list
+        if(number > 2 || factors.isEmpty())
             factors.add(String.valueOf(number));
 
+        // return a string stating the factors of the original number
         return String.join(" * ", factors);
     }
 }
--- main.java
+++ main.java
@@ -1,13 +1,87 @@
-import java.util.Scanner;
-public class Reply_Week5Discussion {
-	public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        long number = (long) input(sc);
-        System.out.println(number);
-    }
+import java.util.*;
 
-    public static double input(Scanner sc) {
-        return sc.nextDouble();
-    }
+public class Week7Discussion {
+	
+	public static void multiplier(int list[], int times){
+
+			list[0] = 1*times;
+			list[1] = 2*times;
+			list[2] = 3*times;
+			list[3] = 4*times;
+			list[4] = 5*times;		
+	}
+	
+	public static void main(String[] args) {
+		
+/**File: Week7Discussion.java 
+
+**Author: Elijah_Gordon
+		
+**Class: CMIS 141 � Week7Discussion
+		
+**Creation Date: April 1, 2021
+		
+**Description: This program was created specifically to allow a user to multiply an array of 5,
+* by any number they choose through a method */
+		
+		Scanner scanint= new Scanner (System.in);
+		
+		int times = 0, choice,index = 0; 
+		int[] list = {1,2,3,4,5};
+		
+		for (int answer = 0; answer < list.length;) {
+			
+		System.out.println("Welcome to the Array multiplier" );
+		
+		System.out.println("\nPlease enter the index you'd like to multiply:" );
+		
+		System.out.println("");
+		index = scanint.nextInt();
+
+		System.out.println("Please enter the number you would like to multipy the index by:" );
+		
+		System.out.println("");
+		times = scanint.nextInt();
+		
+		multiplier (list,times);
+		
+		if (index == 1) {
+		
+System.out.println("\nYou selected Index:"+ " "+ index + ", " +"Multipilied by:"+ " "+ times+ " "+ "resulting in:" +" "+ list[0]);
+					}
+		
+		if (index == 2) {
+			
+System.out.println("\nYou selected Index:"+ " "+ index + ", " +"Multipilied by:"+ " "+ times+ " "+ "resulting in:" +" "+ list[1]);	
+					}	
+		
+		if (index == 3) {
+			
+System.out.println("\nYou selected Index:"+ " "+ index + ", " +"Multipilied by:"+ " "+ times+ " "+ "resulting in:" +" "+ list[2]);
+										
+					}	
+		
+		if (index == 4) {
+			
+System.out.println("\nYou selected Index:"+ " "+ index + ", " +"Multipilied by:"+ " "+ times+ " "+ "resulting in:" +" "+ list[3]);					
+					}	
+		
+		if (index == 5) {
+			
+System.out.println("\nYou selected Index:"+ " "+ index + ", " +"Multipilied by:"+ " "+ times+ " "+ "resulting in:" +" "+ list[4]);
+					}
+		
+System.out.println("\n\nIf you would like to end the program press 9, if not hit any other number to continue:" );
+choice = scanint.nextInt();
+		
+		
+//while (choice==9)
+
+//scanint.close();
+
 }
+}
+}
+
+
+
