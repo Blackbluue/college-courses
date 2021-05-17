609b4562d80e67f45950e53ca4828953de8274c1
blackbluue <>
2021-05-17 18:34:26 +0000

    Work on assignment 1

---
+++ cmis242/Week1/Book.java
@@ -0,0 +1,50 @@
+/**
+ * File: Book.java
+ * Author: Miller, Raymone
+ * Class:  CMIS 242 - Assignment 1
+ * Creation Date: (17MAY21)
+ * Description: Create a Book Inventory interface
+ */
+
+ public class Book {
+    private final int id;
+    private final String title;
+    private final double price;
+
+    /**
+     * Construct a Book object. A negative price will default to 0.
+     *
+     * @param id The unique ID of the book.
+     * @param title The title of the book.
+     * @param price The price of the book.
+     */
+    public Book(int id, String title, double price) {
+        this.id = id;
+        this.title = title;
+        // a negative price will default to 0
+        this.price = price <= 0 ? 0 : price;
+    }
+
+    /**
+     * Return the ID of this book.
+     *
+     * 
+     */
+    public int getId() {
+        return this.id;
+    }
+
+    /**
+     * Return the title of this book.
+     */
+    public String getTitle() {
+        return this.title;
+    }
+
+    /**
+     * Return the price of this book.
+     */
+    public double getPrice() {
+        return this.price;
+    }
+ }
---
+++ cmis242/Week1/MillerRaymone_Asgn1.java
@@ -0,0 +1,13 @@
+/**
+ * File: MillerRaymone_Asgn1.java
+ * Author: Miller, Raymone
+ * Class:  CMIS 242 - Assignment 1
+ * Creation Date: (17MAY21)
+ * Description: Create a Book Inventory interface
+ */
+
+ public class MillerRaymone_Week1Disc {
+    public static void main(String[] args) {
+        
+    }
+ }
